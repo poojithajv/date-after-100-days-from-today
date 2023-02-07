77fec196104d8ccebe505c1a1acba3594a1ec903
poojithajv <poojithajv15@gmail.com>
2023-02-07 06:22:56 +0000

    date after 100 days from today

---
+++ app.js
@@ -0,0 +1,11 @@
+const addDays = require("date-fns/addDays");
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  const result = addDays(new Date(), 100);
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+});
+module.exports = app;
